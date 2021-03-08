10d0e533b98884fae7abade99d3e48eccf6f2716
Beata Mlicka <mlicka.beata@gmail.com>
2021-03-08 13:38:34 +0100

    articles sorting

--- src/components/ArticlesList/ArticlesList.js
+++ src/components/ArticlesList/ArticlesList.js
@@ -2,9 +2,30 @@ import React, { useEffect, useMemo, useState } from 'react'
 import PropTypes from 'prop-types'
 import isEmpty from 'lodash/isEmpty'
 
+import TopBar from './TopBar'
 import Article from './Article'
 
 import './ArticlesList.sass'
+import { orderBy } from 'lodash'
+
+const MONTHS_MAP = {
+  'januar': '01',
+  'februar': '02',
+  'mai': '05',
+  'september': '09',
+  'oktober': '10',
+  'november': '11',
+  'desember': '12',
+}
+
+const getDate = (date) => {
+  const newDate = date.replace('.', '').split(" ")
+  const day = newDate[0]
+  const month = MONTHS_MAP[newDate[1]]
+  const year = newDate[2]
+
+  return { year, month, day }
+}
 
 const ArticlesList = ({
     fashion,
@@ -12,22 +33,34 @@ const ArticlesList = ({
     fetchSportsArticles,
     sport,
 }) => {
+  const [order, setOrder] = useState('desc')
   useEffect(() => {
     fetchFashionArticles()
     fetchSportsArticles()
   }, [])
 
   const articles = useMemo(() => {
-    const articles = []
-    if (!isEmpty(fashion) && !isEmpty(sport)) return [...fashion.articles, ...sport.articles]
+    if (!isEmpty(fashion) && !isEmpty(sport)) {
+        const articlesList = [...fashion.articles, ...sport.articles].map((item) => {
+            return ({
+                ...item,
+                year: getDate(item.date).year,
+                month: getDate(item.date).month,
+                day: getDate(item.date).day,
+            })
+        })
+
+        return orderBy(articlesList, ['year', 'month', 'day'], [order, order, order])
+    }
 
     return []
-  }, [fashion, sport])
+  }, [fashion, sport, order])
 
   if (isEmpty(articles)) return null
   return (
     <div className='articles-list'>
-        {articles.map(item => <Article article={item} />)}
+        <TopBar setOrder={setOrder} />
+        {articles.map(item => <Article key={item.id} article={item} />)}
     </div>
   )
 }
